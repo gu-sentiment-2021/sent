f90d508d58df4368ee23b6af1e0322a2fc65bd1c
arashy76 <arash.r400@gmail.com>
2021-09-25 09:33:19 +0330

    changed code segment for mpqa2json conversion

--- summer21/json2csds/mpqa2json_code_segment.py
+++ summer21/json2csds/mpqa2json_code_segment.py
@@ -2,7 +2,7 @@ from sent.summer21.mpqa_dataprocessing.mpqa3_to_dict import mpqa3_to_dict
 import json
 
 # Specifies the path of the mpqa v3.0 database
-address = "F:\pych\mpqa_3_0_database\database.mpqa.3.0"
+address = "E:\Thesis\mpqa_3_0_database\database.mpqa.3.0"
 
 # Uses the mpqa3_to_dict module to convert mpqa to json file
 m2d = mpqa3_to_dict("MPQA3.0", address)
