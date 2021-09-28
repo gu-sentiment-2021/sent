f346bed7ea1b42e8c4d55a079cc6303a647e8dc5
arash.r400@gmail.com <arash.r400@gmail.com>
2021-09-28 13:12:13 +0330

    Target collection class added

--- summer21/json2csds/Target.py
+++ summer21/json2csds/Target.py
@@ -51,3 +51,73 @@ class sTarget(Target):
         Target.__init__(self, this_id, this_span_start, this_span_end)
         self.target_uncertain = this_target_uncertain
         self.etarget_link = this_etarget_link
+
+
+class TargetCollection:
+    """
+    Holds a collection of Target objects for a single corpus, each of which represents
+    a single Target annotation in the corpus.
+    """
+    # List of target objects which are made from target annotations from the mpqa corpus.
+    target_instances = []
+
+    # A python dict that is used for indexing the target objects based on their ID
+    indexer = dict()
+
+    # Name of the corpus from which the objects in this collection were drawn.
+    # This collection represents a single corpus.
+    corpus = ""
+
+    def __init__(self, this_corpus):
+        """
+        Stores the name of the corpus from which the objects are drawn.
+        :param this_corpus:
+        """
+        self.corpus = this_corpus
+
+    def add_instance(self, new_instance):
+        """
+        Adds a single Target object to the collection for an actual annotation.
+        :param new_instance: The Target object representing the Target annotation.
+        :return: None.
+        """
+        # Add the new Target instance to the list
+        self.target_instances.append(new_instance)
+
+        # Index the new Target instance based on its ID
+        self.indexer[new_instance.target_id] = len(self.target_instances)
+
+    def get_instance(self, instance_id):
+        """
+        Returns a single Target object from the collection by its ID, if not exists, None will be returned.
+        :param instance_id: The ID of the Target annotation which is desired to be received.
+        :return: None.
+        """
+        # Check if the instance_id is available in the collection by using the indexer in O(1) average time complexity
+        if instance_id in self.indexer:
+            # Return the corresponding Target object by using its ID.
+            return self.target_instances[self.indexer[instance_id]]
+        return None
+
+    # Under construction: The following method is not using the storage efficiently! But it is not being used very much.
+    def del_instance(self, instance_id):
+        """
+        Deletes a single Target object from the collection by its ID.
+        :param instance_id: The ID of the Target annotation which is desired to be deleted.
+        :return: None.
+        """
+        # Check if the instance_id is available in the collection by using the indexer in O(1) average time complexity
+        if instance_id in self.indexer:
+            # Put None in the corresponding place of the object which is going to be deleted.
+            self.target_instances[self.indexer[instance_id]] = None
+
+            # Delete the index value of the object which is going to be deleted.
+            del self.indexer[instance_id]
+
+    def reset_collection(self):
+        """
+        Reset (set empty) the Target objects collection.
+        :return: None.
+        """
+        self.indexer = dict()
+        self.target_instances = []
