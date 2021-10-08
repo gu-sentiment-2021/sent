5334f365c4b8e12850412732d9dbfb083aa96bdc
mahsahsi <mahsahsii@gmail.com>
2021-10-09 00:34:48 +0330

    highlight head in output of tokenizer - test1

--- summer21/ext_csds2hf/ext_csds2hf_M.py
+++ summer21/ext_csds2hf/ext_csds2hf_M.py
@@ -44,7 +44,57 @@ class extCsds2hfV1:
         csds_objs = csds_coll.get_all_instances()
         return csds_objs[0]
 
+def findHead(sequence, head_text):
+    symbols = ['.', '!', ',', '?']
+    a = -1
+    b = -1
+    j = 0
 
+    for symbol in symbols:
+        sequence = sequence.replace(symbol, '')
+    tokenized_sequence = sequence.split()
+    print(tokenized_sequence)
+
+    tokenized_head_text = head_text.split()
+    print(tokenized_head_text)
+
+    for i in range(len(tokenized_sequence)):
+        if j <= len(tokenized_head_text):
+            if tokenized_head_text[j] == tokenized_sequence[i]:
+                if j == 0:
+                    a = i
+                if j == len(tokenized_head_text) - 1:
+                    b = i
+                j += 1
+            else:
+                j = 0
+                a = -1
+        else:
+            break
+
+    return a, b
+
+def findHeadInToken(tokens, a, b):
+    symbols = ['.', '!', ',', '?']
+    c = -1
+    d = -1
+    j = 0
+    for i in range(len(tokens)):
+        flag = True
+        if j == a:
+            c = i
+        if j == b:
+            d = i
+        if tokens[i].startswith('##'):
+            j = j
+        else:
+            for symbol in symbols:
+                if tokens[i] == symbol:
+                    flag = False
+            if flag == True:
+                j += 1
+
+    return c, d
 # test part
 address = "..\mpqa_dataprocessing\databases\database.mpqa.3.0.cleaned"
 obj = extCsds2hfV1("MPQA3.0", address)
@@ -54,7 +104,21 @@ csds_objects = obj.extract_csds_objects(tuple_res)
 
 print(csds_objects[0])
 
-checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+checkpoint = "bert-base-cased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+sequence = "Using a Transformer network is simple."
+head_text = "is simple"
+tokens = tokenizer.tokenize(sequence)
+print(tokens)
+
+# for highlight head in tokens
+# find head in sequence
+a, b = findHead(sequence, head_text)
+print(a, ',', b)
+# find pos of head in tokens
+c, d = findHeadInToken(tokens, a, b)
+print(c, ',', d)
+for i in range(c, d + 1):
+    print(tokens[i])
 # model_inputs = tokenizer(sequence)
