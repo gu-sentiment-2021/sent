a7882b0c430639c5a7ee5fc723d1efe0c31c5326
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2021-11-14 11:11:14 +0330

    Added mpqa2_to_dict

---
+++ summer21/mpqa_dataprocessing/mpqa2_to_dict_code_segment.py
@@ -0,0 +1,14 @@
+# By running this code segment you'll get a python dictionary or
+# a json file that helps you to further examine the MPQA 3.0 dataset
+from mpqa_dataprocessing.mpqa2_to_dict import mpqa2_to_dict
+import json
+
+# Specifies the path of the mpqa v3.0 database
+address = "database.mpqa.cleaned"
+
+# Uses the mpqa3_to_dict module to convert mpqa to json file
+m2d = mpqa2_to_dict("MPQA2.0", address)
+result = m2d.corpus_to_dict()
+# Save the json file to a path
+with open("result_mpqa3.json", 'w') as file:
+    json.dump(result, file, indent=4)
