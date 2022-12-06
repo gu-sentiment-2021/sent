a3dc41c957d2dde6d543e9e65a9260924f2a85fd
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2022-12-06 10:06:48 +0330

    mpqa2 code segment

--- summer21/mpqa_dataprocessing/mpqa2_to_dict_code_segment.py
+++ summer21/mpqa_dataprocessing/mpqa2_to_dict_code_segment.py
@@ -1,14 +1,27 @@
 # By running this code segment you'll get a python dictionary or
 # a json file that helps you to further examine the MPQA 3.0 dataset
-from mpqa_dataprocessing.mpqa2_to_dict import mpqa2_to_dict
+# from mpqa_dataprocessing.mpqa2_to_dict import mpqa2_to_dict
+from mpqa2_to_dict import mpqa2_to_dict
 import json
 
 # Specifies the path of the mpqa v2.0 database
-address = "database.mpqa.cleaned"
+address = "mpqa_dataprocessing\\database.mpqa.cleaned.221201"
 
 # Uses the mpqa2_to_dict module to convert mpqa to json file
 m2d = mpqa2_to_dict("MPQA2.0", address)
 result = m2d.corpus_to_dict()
 # Save the json file to a path
-with open("result_mpqa2.json", 'w') as file:
+with open("result_mpqa2_cleaned.json", 'w') as file:
     json.dump(result, file, indent=4)
+
+##### Find and count OSEs without a sentence
+# x, y = 0, 0
+# for docname, doc in result['docs'].items():
+#     for annoname in doc['objective-speech-event']:
+#         for anno in doc['annotations'][annoname]:
+#             if 'span-in-sentence' not in anno:
+#                 x += 1
+#                 print(docname, annoname)
+#                 if 'implicit' not in anno:
+#                     y += 1
+# print(x, y)
