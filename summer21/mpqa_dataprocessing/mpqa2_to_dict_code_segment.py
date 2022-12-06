1e7d11389c71479e12594136149d32b4e63afb3e
Saeed-Ahmadnia <saeed.ahmadnia@outlook.com>
2022-12-06 15:56:22 +0330

    More counters

--- summer21/mpqa_dataprocessing/mpqa2_to_dict_code_segment.py
+++ summer21/mpqa_dataprocessing/mpqa2_to_dict_code_segment.py
@@ -14,14 +14,42 @@ result = m2d.corpus_to_dict()
 with open("result_mpqa2_cleaned.json", 'w') as file:
     json.dump(result, file, indent=4)
 
-##### Find and count OSEs without a sentence
+# ##### Find and count OSEs without a sentence
 # x, y = 0, 0
 # for docname, doc in result['docs'].items():
-#     for annoname in doc['objective-speech-event']:
+#     for annoname in doc['direct-subjective']:
 #         for anno in doc['annotations'][annoname]:
 #             if 'span-in-sentence' not in anno:
 #                 x += 1
 #                 print(docname, annoname)
-#                 if 'implicit' not in anno:
-#                     y += 1
+#                 # if 'implicit' not in anno:
+#                 #     y += 1
 # print(x, y)
+
+# ##### Count All annotations
+# annos = {}
+# for docname, doc in result['docs'].items():
+#     for annotype, annonames in doc.items():
+#         if annotype != 'annotations':
+#             annos[annotype] = annos.get(annotype, 0) + len(annonames)
+# print(annos)
+
+# ##### Count number of links in attitudes, DSEs, and ESEs.
+# trgt_in_att, src_in_dse, att_in_dse, src_in_ese = 0, 0, 0, 0
+# for docname, doc in result['docs'].items():
+#     for annoname in doc['attitude']:
+#         for anno in doc['annotations'][annoname]:
+#             if 'target-link' in anno:
+#                 trgt_in_att += len(anno['target-link'])
+#     for annoname in doc['direct-subjective']:
+#         for anno in doc['annotations'][annoname]:
+#             if 'nested-source' in anno:
+#                 src_in_dse += len(anno['nested-source'])
+#             if 'attitude-link' in anno:
+#                 att_in_dse += len(anno['attitude-link'])
+#     for annoname in doc['expressive-subjectivity']:
+#         for anno in doc['annotations'][annoname]:
+#             if 'nested-source' in anno:
+#                 src_in_ese += len(anno['nested-source'])
+#
+# print(trgt_in_att, src_in_dse, att_in_dse, src_in_ese)
